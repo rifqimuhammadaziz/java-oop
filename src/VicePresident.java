cfed7ee96eb8f8c0ccebad56e763a43e5029b14f
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-10 05:50:55 +0700

    add method overriding

--- src/VicePresident.java
+++ src/VicePresident.java
@@ -2,5 +2,11 @@
  * Inheritance : have all attribute/methods from parent
  */
 public class VicePresident extends Manager {
-
+    /**
+     * Overriding : create same method of parent (to replace method from parent)
+     * @param name
+     */
+    void sayHello(String name) {
+        System.out.println("Hi " + this.name + ", My Name is VP " + name);
+    }
 }
