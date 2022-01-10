6fc1b20cda003bbb15d759a1840bba3201128fb4
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-10 20:01:17 +0700

    add default method on interface

--- src/Interface/Car.java
+++ src/Interface/Car.java
@@ -3,4 +3,12 @@ package Interface;
 public interface Car extends HasBrand, IsMaintenance {
     void drive();
     int getTier();
+
+    /**
+     * Default method : child/inherited class not mandatory to override method (java 8++)
+     * @return
+     */
+    default boolean isBig() {
+        return false;
+    }
 }
