1c4fa827245947c7cc6712c5664c22719f0546fc
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-12 05:43:40 +0700

    add annotaion/metadata

---
+++ src/annotation/FancyApp.java
@@ -0,0 +1,9 @@
+package annotation;
+
+/** Annotation : to add information (metadata) about class/method */
+@Fancy(name = "Fancy Application", tags = {"application", "java"})
+public class FancyApp {
+    public static void main(String[] args) {
+
+    }
+}
